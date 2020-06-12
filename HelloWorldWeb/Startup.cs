fbde283291285c3a33aba881d40e176fad49b770
soumik bhattacharjee <soumik_bhattacharjee@hotmail.com>
2020-06-12 16:56:52 +0530

    s2i update v2

--- HelloWorldWeb/Startup.cs
+++ HelloWorldWeb/Startup.cs
@@ -36,23 +36,24 @@ namespace HelloWorldWeb
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
+             public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+             {
+                 if (env.IsDevelopment())
+                 {
+                     app.UseDeveloperExceptionPage();
+                 }
+                 else
+                 {
+                     app.UseExceptionHandler("/Error");
+                     app.UseHsts();
+                 }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
+                 app.UseHttpsRedirection();
+                 app.UseStaticFiles();
+                 app.UseCookiePolicy();
 
-            app.UseMvc();
-        }
+                 app.UseMvc();
+             }
+
     }
 }
